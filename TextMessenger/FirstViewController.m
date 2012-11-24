d83e9f698d727e84e5ba63145c0fdbd337511f8e
Lichard Baliuag <lichardbaliuag@Lichard-Baliuags-MacBook-Pro.local>
2012-11-24 23:03:48 +0800

    Initial Commit

---
+++ TextMessenger/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  TextMessenger
+//
+//  Created by Lichard Baliuag on 24/11/12.
+//  Copyright (c) 2012 Lichard Baliuag. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TextMessenger/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  TextMessenger
+//
+//  Created by Lichard Baliuag on 24/11/12.
+//  Copyright (c) 2012 Lichard Baliuag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
